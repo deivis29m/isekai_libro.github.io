0c618ce70e27fa3313ccc836dadd9d538114c5d9
Deivis28m <117247414+Deivis28m@users.noreply.github.com>
2023-04-23 13:50:42 -0500

    punto de contro

---
+++ fn.js
@@ -0,0 +1,43 @@
+const section = document.getElementById("section");
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+const boxes = section.querySelectorAll(".box");
+let currentBoxIndex = 0;
+
+// Oculta todas las cajas excepto la actual
+function hideBoxes() {
+  boxes.forEach((box) => {
+    if (box !== boxes[currentBoxIndex]) {
+      box.style.display = "none";
+    } else {
+      box.style.display = "flex";
+    }
+  });
+}
+
+// Muestra la siguiente caja
+function showNextBox() {
+  if (currentBoxIndex < boxes.length - 1) {
+    currentBoxIndex++;
+  } else {
+    currentBoxIndex = 0;
+  }
+  hideBoxes();
+}
+
+// Muestra la caja anterior
+function showPrevBox() {
+  if (currentBoxIndex > 0) {
+    currentBoxIndex--;
+  } else {
+    currentBoxIndex = boxes.length - 1;
+  }
+  hideBoxes();
+}
+
+// Agrega los eventos a los botones
+nextBtn.addEventListener("click", showNextBox);
+backBtn.addEventListener("click", showPrevBox);
+
+// Oculta todas las cajas excepto la primera
+hideBoxes();
